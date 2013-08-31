9e92d336b263965750cb165fb3bce20131870835
PaulaEs <paulaproxy@gmail.com>
2013-08-31 21:05:42 +0200

    Destroying and rebuilding migration and model section.

---
+++ db/migrate/20130831185555_create_sections.rb
@@ -0,0 +1,16 @@
+class CreateSections < ActiveRecord::Migration
+  def change
+    create_table :sections do |t|
+    	t.string :name
+    	t.integer :position
+    	t.boolean :visible
+    	t.string :content_type
+    	t.text :content
+    	t.references :page 
+
+      t.timestamps
+    end
+
+    add_index :sections, :page_id
+  end
+end
