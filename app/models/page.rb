66d566923d6d3cd33265ac2ede891ced3725e19e
PaulaEs <paulaproxy@gmail.com>
2013-09-03 01:14:56 +0200

    Admin_users and pages association. Join table added.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,5 @@
 class Page < ActiveRecord::Base
   belongs_to :subject
+  has_many :sections
+  has_and_belongs_to_many :admin_users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130831185555) do
+ActiveRecord::Schema.define(version: 20130902223944) do
 
   create_table "admin_users", force: true do |t|
     t.string   "first_name",      limit: 25
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20130831185555) do
   end
 
   add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
+
+  create_table "admin_users_pages", id: false, force: true do |t|
+    t.integer "admin_user_id"
+    t.integer "page_id"
+  end
+
+  add_index "admin_users_pages", ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id", using: :btree
 
   create_table "pages", force: true do |t|
     t.string   "name"
