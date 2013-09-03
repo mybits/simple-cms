31c603c9f10141561b8882324f7005fdda13899c
PaulaEs <paulaproxy@gmail.com>
2013-09-04 00:30:49 +0200

    Subject controller and view.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,11 @@
 class SubjectsController < ApplicationController
+
+	def index
+		@subjects = Subject.order('subjects.position ASC')
+	end
+
+	def show
+		@subject = Subject.find(params[:id])
+	end
+
 end
