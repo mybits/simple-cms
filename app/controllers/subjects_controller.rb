55408c75e17f4498d13ec50c651f1fcc72420c05
PaulaEs <paulaproxy@gmail.com>
2013-09-05 02:25:53 +0200

    Create and update fro subjects.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,4 +8,32 @@ class SubjectsController < ApplicationController
 		@subject = Subject.find(params[:id])
 	end
 
+	def new
+		@subject = Subject.new
+	end
+
+	def create
+		@subject = Subject.new      # (params[:subject]) - PROBLEM - nie wchodzą parametry
+		if @subject.save
+			redirect_to action: 'index'
+		else
+			render action: 'new'
+		end
+	end
+
+	def edit
+		@subject = Subject.find(params[:id])
+	end
+
+	def update
+		@subject = Subject.find(params[:id])
+		if @subject.update_columns(params[:subject])
+			redirect_to action: 'show'
+		else
+			render action: 'new'
+		end
+
+	end
+
+
 end
