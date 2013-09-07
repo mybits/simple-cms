cfd99e6416595c695ad0f89aa5a2cb23d70da6d5
PaulaEs <paulaproxy@gmail.com>
2013-09-07 22:02:52 +0200

    Add new and create to sections.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -7,4 +7,26 @@ class SectionsController < ApplicationController
 	def show
 		@section = Section.find(params[:id])
 	end
+
+	def new
+		@section = Section.new
+	end
+
+	def create
+		@section = Section.new(section_params)
+		if @section.save
+			redirect_to action: 'index'
+		else
+			render action 'new'
+		end
+	end
+
+
+	private
+
+		def section_params
+			params.require(:section).permit(:name, :position, :visible, :content)
+		end
+
+
 end
