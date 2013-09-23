dabd38caaacaaceb5162e041180a8783f38f3f7f
PaulaEs <paulaproxy@gmail.com>
2013-09-24 00:02:37 +0200

    Hashing password.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,12 +1,14 @@
+# require 'digest/sha1'
+
 class AdminUser < ActiveRecord::Base
 	has_and_belongs_to_many :pages 
 	has_many :section_edits
 	has_many :sections, through: :section_edits
 
-	EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+	EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
 
 	validates :first_name, presence: true, length: { maximum: 25 }
-	validates :last_name, presence: true, lemght: { maximum: 50 }
+	validates :last_name, presence: true, length: { maximum: 50 }
 	validates :username, presence: true, length: { within: 5..25 }, uniqueness: true
 	validates :email, presence: true,
 										length: { maximum: 100 },
@@ -15,4 +17,9 @@ class AdminUser < ActiveRecord::Base
 										confirmation: true
 
 	# scope :named, lambda {|first, last| where(first_name: first, last_name: last)}
+
+
+	def self.hash(password="")
+		Digest::SHA1.hexdigest(password)
+	end
 end
