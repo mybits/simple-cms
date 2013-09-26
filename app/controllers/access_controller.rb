5016d939050b9ef77c5b012ffa058f78e160fd72
PaulaEs <paulaproxy@gmail.com>
2013-09-27 00:52:55 +0200

    Login, logout methods.

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,5 +1,7 @@
 class AccessController < ApplicationController
 
+	layout 'admin'
+
 	def index
 		menu
 		render 'menu'
@@ -14,10 +16,21 @@ class AccessController < ApplicationController
   end
 
   def attempt_login
+  	authorized_user = AdminUser.authenticate(params[:username], params[:password])
+  	if authorized_user
+  		# TODO: mark user as logged in
+  		flash[:notice] = "You are now logged in."
+  		redirect_to action: 'menu'
+  	else
+  		flash[:notice] = "Invalid username/password combination"
+  		redirect_to action: 'login'
+  	end
   end
 
   def logout
+  	# TODO: mark user as logged out
+  	flash[:notice] = "You have been logged out."
+  	redirect_to action: 'login'
   end
-
 
 end
